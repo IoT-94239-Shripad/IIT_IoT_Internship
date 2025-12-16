4aaf192667a1450b336114880db02b5c0f98d560
kulkarnishripadsrk <shripad.srk77@gmail.com>
2025-12-16 08:58:29 +0530

    python assignment

--- python assignment/q10.py
+++
@@ -1,6 +0,0 @@
-def print_binary(num):
-    binary = bin(num)[2:]   
-    print("Binary representation:", binary)
-
-n = int(input("Enter a number: "))
-print_binary(n)
--- python assignment/q16.py
+++
@@ -1,7 +0,0 @@
-def greet(name, greeting="Hello"):
-    print(f"{greeting}, {name}")
-
-greet("Raj")
-
-greet("Rohan", "Hi")
-
