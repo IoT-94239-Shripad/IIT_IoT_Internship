c64e88c6caeb116404bc7e5060f6f65bd4f1dd7a
kulkarnishripadsrk <shripad.srk77@gmail.com>
2025-12-16 09:05:43 +0530

    python assignment

--- python assignment/day 2 q6.py
+++
@@ -1,8 +0,0 @@
-
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number (divider): "))
-if num2 == 0:
-    print("Error: Division by zero is not allowed.")
-else:
-    result = num1 / num2
-    print("Division result =", result)
