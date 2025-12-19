d38a6333c7041ac5ec35ab44303900503234a847
kulkarnishripadsrk <shripad.srk77@gmail.com>
2025-12-19 09:55:08 +0530

    day 6

--- day 6/q1.py
+++ day 6/q1.py
@@ -3,33 +3,29 @@ from datetime import datetime
 
 # establish connection with mysql server
 connection = mysql.connector.connect(
-    host = "127.0.0.1",
-    port = 3306,
-    user = "root",
-    password = "root",
-    database = "iot_data2",
-    use_pure = True
+    host="127.0.0.1",
+    port=3306,
+    user="root",
+    password="root",
+    database="iot_data2",
+    use_pure=True
 )
+
 id = int(input("Enter id : "))
 temperature = float(input("Enter temperature : "))
-humidity= float(input("Enter Humidity : "))
+humidity = float(input("Enter Humidity : "))
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-query = f"insert into sensor_readings({id}, '{temperature}', '{humidity}', '{timestamp}');"
+query = """
+INSERT INTO sensor_readings   (id, temperature, humidity, timestamp)
+VALUES (%s, %s, %s, %s)
+"""
 
-# create a cursor to execute a query
 cursor = connection.cursor()
-
-# execute a query
-cursor.execute(query)
-
-# commit your changes on mysql server
+cursor.execute(query, (id, temperature, humidity, timestamp))
 connection.commit()
 
-# close the cursor
 cursor.close()
-
-# close the connection with mysql server
 connection.close()
 
-
+print("Record inserted successfully")
