ed18bae20e21a98924699eab8a5ca33bd46f8e4e
kulkarnishripadsrk <shripad.srk77@gmail.com>
2025-12-23 08:50:50 +0530

    day 6

---
+++ day 6/q3.py
@@ -0,0 +1,41 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/send_temperature')
+def send_temperature():
+    temp = request.args.get('value')
+
+    with open("temperature.txt", "a") as f:
+        f.write(temp + "\n")
+
+    return "Temperature stored successfully"
+
+
+
+@app.route('/send_light')
+def send_light():
+    light = request.args.get('value')
+
+    with open("light.txt", "a") as f:
+        f.write(light + "\n")
+
+    return "Light intensity stored successfully"
+
+@app.route('/get_temperature')
+def get_temperature():
+    with open("temperature.txt", "r") as f:
+        data = f.read()
+    return "<pre>" + data + "</pre>"
+
+
+@app.route('/get_light')
+def get_light():
+    with open("light.txt", "r") as f:
+        data = f.read()
+    return "<pre>" + data + "</pre>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
